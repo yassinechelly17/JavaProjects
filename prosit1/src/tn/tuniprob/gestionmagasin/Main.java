c19a0f79c9963e8c5bb53290217fb03bbeb3d8c8
Mohamed Yassine Chelly <yassinechelly@ieee.org>
2024-09-27 15:56:39 +0100

    Added Prosit 2

---
+++ prosit1/src/tn/tuniprob/gestionmagasin/Main.java
@@ -0,0 +1,69 @@
+package prosit1.gestion_Magasin.tn.tuniprob.gestionmagasin;
+import java.util.Date;
+
+class Main {
+    public static void main(String[] args) {
+
+        // question 1
+        Produit p_vide = new Produit();
+
+        // question 2
+            //  p1
+        Produit p1 = new Produit(1021, "Lait", "Delice");
+            //  p2
+        Produit p2 = new Produit(2510, "Yaourt", "Vitalait");
+            // p3
+        Produit p3 = new Produit(3250, "Tomate", "Sicam", 1.200);
+
+        // question 3
+        p1.afficher();
+        p2.afficher();
+        p3.afficher();
+
+        System.out.println("");
+        System.out.println("-----------------");
+        System.out.println("Valeurs modfiées:");
+
+        // question 4
+        p1.setPrix(0.800);
+        p1.afficher();
+
+        // question 5
+        p2.setPrix(0.500);
+        // question 6
+        p2.afficher();
+
+        // question 7
+        System.out.println("");
+        System.out.println("-----------------");
+        System.out.println("Affichés avec toString():");
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+
+        p1.setDateExpiration(new Date(2024, 9, 31));
+        p2.setDateExpiration(new Date(2024, 10, 10));
+
+        System.out.println("-----------------");
+        Magasin m1 = new Magasin();
+        m1.id = 1;
+        m1.adresse = "Tunis";
+        m1.ajouterProduit(p1);
+        m1.ajouterProduit(p2);
+        Magasin m2 = new Magasin();
+        m2.id = 2;
+        m2.adresse = "Ariana";
+        m2.ajouterProduit(p3);
+        System.out.println(m1.toString());
+        System.out.println(m2.toString());
+
+
+
+
+
+
+        return;
+    }
+}
+
+
---
+++ prosit1/src/tn/tuniprob/gestionmagasin/Produit.java
@@ -0,0 +1,85 @@
+package prosit1.gestion_Magasin.tn.tuniprob.gestionmagasin;
+
+import java.util.Date;
+
+public class Produit {
+    private int id;
+    private String libelle;
+    private String marque;
+    private double prix;
+    private Date date_expiration;
+
+    public Produit () {
+        this.id = 0;
+        this.libelle = "";
+        this.marque = "";
+        this.prix = 0.0;
+    }
+    public Produit(int id, String libelle, String marque, double prix) {
+        this.id = id;
+        this.libelle = libelle;
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+    public Produit(int id, String libelle, String marque){
+        this.id = id;
+        this.libelle = libelle;
+        this.marque = marque;
+        this.prix = 0.0;
+    }
+
+    public void afficher() {
+        System.out.println("Le produit " + this.libelle + " de la marque " + this.marque + " a pour prix " + this.prix);
+        return;
+    }
+
+
+    // public String toString() {
+     //   return "Produit [id=" + this.id + ", libelle=" + this.libelle + ", marque=" + this.marque + ", prix=" + this.prix + "]";
+   // }
+
+    public String toString(){
+        return "Produit [marque=" + this.marque + ", prix=" + this.prix + "]";
+    }
+
+    // Getters
+    public int getId() {
+        return this.id;
+    }
+    public String getLibelle() {
+        return this.libelle;
+    }
+    public String getMarque() {
+        return this.marque;
+    }
+    public double getPrix() {
+        return this.prix;
+    }
+    public Date getDateExpiration() {
+        return this.date_expiration;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+    public void setPrix(double prix) {
+        if (prix < 0) {
+            System.out.println("Le prix ne peut pas être négatif");
+            return;
+        }
+        this.prix = prix;
+    }
+    public void setDateExpiration(Date date_expiration) {
+        this.date_expiration = date_expiration;
+    }
+
+}
+
